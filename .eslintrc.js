0a36cfb0116939d7331671e7127acf415dc4d173
Bbanez <banezprod@gmail.com>
2022-05-13 16:21:15 +0200

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,11 @@ module.exports = {
   },
   extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended'],
   plugins: [],
-  // add your custom rules here
   rules: {
     'no-debugger': 'warn',
     'no-shadow': 'error',
     'no-unused-labels': 'error',
     'no-unused-expressions': 'error',
-    'no-duplicate-imports': 'error',
-    'require-await': false
+    'no-duplicate-imports': 'error'
   }
 }
