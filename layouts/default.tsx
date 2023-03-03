d830b8ea8655d0c0188fe31c32e0415f54825f3a
Bbanez <banezprod@gmail.com>
2023-03-03 17:24:24 +0100

    Update packages and fix env init

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -2,7 +2,7 @@ import '../styles/_main.scss';
 import { BCMSImageConfig } from '@becomes/cms-most/frontend';
 
 BCMSImageConfig.cmsOrigin = process.env.VITE_BCMS_API_ORIGIN || '';
-BCMSImageConfig.publicApiKeyId = process.env.VITE_BCMS_API_KEY || '';
+BCMSImageConfig.publicApiKeyId = process.env.VITE_BCMS_PUB_API_KEY || '';
 BCMSImageConfig.localeImageProcessing = false;
 
 export default defineNuxtComponent({
