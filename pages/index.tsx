3f7f69f358806122928d81ac5b0b653ab4687063
Bbanez <banezprod@gmail.com>
2023-07-06 13:31:19 +0200

    fix: lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,25 @@
-import { NuxtLink } from "#components";
+import { NuxtLink } from '#components';
 
 export default defineNuxtComponent({
   setup() {
     const { data } = useAsyncData(async (ctx) => {
       const templates = await ctx?.$bcms.template.getAll();
-      return templates?.map((e) => {
-        return {
-          title: e.label,
-          slug: e.name,
-        };
-      });
+      const result =
+        templates?.map((e) => {
+          return {
+            title: e.label,
+            slug: e.name,
+          };
+        }) || [];
+      return result;
     });
 
     return () => (
       <div>
         <NuxtLink href="/">Home</NuxtLink>
         <ul>
-          {data.value?.map((item) => (
-            <li>
+          {data.value?.map((item, itemIdx) => (
+            <li key={itemIdx}>
               <NuxtLink href={'/' + item.slug}>{item.title}</NuxtLink>
             </li>
           ))}
